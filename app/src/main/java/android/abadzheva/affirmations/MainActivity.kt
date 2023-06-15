bc06b1feb1f26ae4151aa278d12a780c7ef53f96
AbadzhevaK8 <make.me.loco@gmail.com>
2023-06-15 22:04:06 +0300

    Initial commit

---
+++ app/src/main/java/android/abadzheva/affirmations/MainActivity.kt
@@ -0,0 +1,11 @@
+package android.abadzheva.affirmations
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
